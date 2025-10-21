b2b3bdacea05f858d6456faf35ff14ac9bda7182
Marwan Yasser <marwan.elsokary12234@gmail.com>
2025-10-21 04:35:31 +0300

    feat[App Icon]: - build App Icon - handle main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:fitness_app/core/di/di.dart';
-// import 'package:fitness_app/core/extensions/project_extensions.dart';
-// import 'package:fitness_app/core/l10n/app_localizations.dart';
 import 'package:fitness_app/core/route/app_routes.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'core/route/routes.dart';
-//import 'core/theme/app_theme.dart';
 import 'core/utils/caching/caching_helper.dart';
-// import 'core/utils/language_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,45 +23,14 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 813),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Fitness App",
-        theme: ThemeData.dark(),
-        onGenerateRoute: Routes.generateRoute,
-        initialRoute: AppRoutes.navBar,
-      ),
+      builder: (context, child) =>
+          MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Fitness App",
+            theme: ThemeData.dark(),
+            onGenerateRoute: Routes.generateRoute,
+            initialRoute: AppRoutes.navBar,
+          ),
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key, this.token});
-
-//   final String? token;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ScreenUtilInit(
-//       designSize: const Size(375, 813),
-//       minTextAdapt: true,
-//       splitScreenMode: true,
-//       builder: (context, child) => BlocProvider(
-//         create: (context) => getIt<LocaleCubit>(),
-//         child: BlocBuilder<LocaleCubit, Locale>(
-//           builder: (context, locale) {
-//             return MaterialApp(
-//               debugShowCheckedModeBanner: false,
-//               title: context.l10n.appName,
-//               localizationsDelegates: AppLocalizations.localizationsDelegates,
-//               supportedLocales: AppLocalizations.supportedLocales,
-//               locale: locale,
-//               theme: AppTheme.lightTheme,
-//               onGenerateRoute: Routes.generateRoute,
-//               initialRoute: AppRoutes.homeScreen,
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
