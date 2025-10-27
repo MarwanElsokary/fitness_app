fe82ad9d7a1cd7cad203951abe5db5411b626c56
lbarsidati22 <lbarsidati575@gmail.com>
2025-10-27 20:14:35 +0000

    finish task

--- lib/core/api_layer/api_client/endpoints.dart
+++ lib/core/api_layer/api_client/endpoints.dart
@@ -11,21 +11,8 @@ class Endpoints {
   static const String getAllDifficultyLevelsByPrimeMoverMuscle =
       "levels/difficulty-levels/by-prime-mover";
 
-  //////////////////////////// Muscle Routes ////////////////////////////
   static const String allMuscles = "/muscles";
   static const String allMusclesGroup = "/musclesGroup";
-  static const String musclesRandom = "muscles/random";
-  static const String musclesGroupByMuscleGroup =
-      "musclesGroup/by-muscle-group";
-
-  //////////////////////////// Meals Routes ////////////////////////////
   static const String mealsCategories = "/1/categories.php";
-  static const String filterMealsByCategory = "1/filter.php";
-  static const String mealDetails = "1/lookup.php";
-
-  /////////////////////////// Exercises Routes ////////////////////////////
-  static const String allExercises = "exercises";
-  static const String exercisesByMuscleDifficulty =
-      "exercises/by-muscle-difficulty";
   static const String exercisesByRandom = "/exercises/random";
 }
