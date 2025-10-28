ec011165238b0d0742e32e9c731467cddda0947d
Marwan Yasser <marwan.elsokary12234@gmail.com>
2025-10-28 04:10:36 +0300

    Add Codecov integration - fix firbase app distribution

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: "Fitness App ",
+        title: "Fitness App",
         theme: ThemeData.dark(),
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
